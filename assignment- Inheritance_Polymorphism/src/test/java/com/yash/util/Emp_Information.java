ea2b850ac024aadab435d5c54d14889254ebb1a0
maurya360 <79659368+maurya360@users.noreply.github.com>
2021-08-17 21:48:04 +0530

    Add files via upload

---
+++ assignment- Inheritance_Polymorphism/src/test/java/com/yash/util/Emp_Information.java
@@ -0,0 +1,15 @@
+package com.yash.util;
+
+public class Emp_Information {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Employee p= new Employee(20000,13,"1232","moka");
+		p.Emp_Information();
+		
+		
+
+	}
+
+}
